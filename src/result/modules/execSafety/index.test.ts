74a7de9c40a41167b58c7873fd91216c8dd40a9b
Shin-Taro <sh.takahashi.113@gmail.com>
2024-04-01 22:36:28 +0900

    feat: currying favour execSafety

--- src/result/modules/execSafety/index.test.ts
+++ src/result/modules/execSafety/index.test.ts
@@ -2,14 +2,14 @@ import { execSafety } from "."
 
 describe("execSafety", () => {
   test("should return a success result", () => {
-    const result = execSafety(() => 42)
-    expect(result).toEqual({ isSuccess: true, data: 42 })
+    const result = execSafety((num: number) => 42 + num)(10)
+    expect(result).toEqual({ isSuccess: true, data: 52 })
   })
 
   test("should return a failure result", () => {
     const result = execSafety(() => {
       throw new Error("Something went wrong")
-    })
+    })()
     expect(result).toEqual({ isSuccess: false, error: new Error("Something went wrong") })
   })
 })
--- src/result/modules/execSafety/index.ts
+++ src/result/modules/execSafety/index.ts
@@ -1,19 +1,18 @@
 import { Result } from "../../types/Result"
 import { createResult } from "../createResult"
 
-export const execSafety = <Data, Args extends unknown[]>(
-  fn: (...args: Args) => Data,
-  ...args: Args
-): Result<Error, Data> => {
-  const { createFailure, createSuccess } = createResult<Error, Data>()
+export const execSafety =
+  <Data, Args extends unknown[]>(fn: (...args: Args) => Data) =>
+  (...args: Args): Result<Error, Data> => {
+    const { createFailure, createSuccess } = createResult<Error, Data>()
 
-  try {
-    const data = fn(...args)
-    return createSuccess(data)
-  } catch (error) {
-    if (error instanceof Error) {
-      return createFailure(error)
+    try {
+      const data = fn(...args)
+      return createSuccess(data)
+    } catch (error) {
+      if (error instanceof Error) {
+        return createFailure(error)
+      }
+      return createFailure(new Error("Unknown error occurred"))
     }
-    return createFailure(new Error("Unknown error occurred"))
   }
-}
