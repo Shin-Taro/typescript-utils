acd6bd065d5cdb711a55bfd15cfc5d88e8688f15
Shin-Taro <sh.takahashi.113@gmail.com>
2024-04-01 22:37:14 +0900

    refactor: use createResult in mapResult

--- src/result/modules/mapResult/index.ts
+++ src/result/modules/mapResult/index.ts
@@ -1,10 +1,13 @@
 import { Result } from "../../types/Result"
+import { createResult } from "../createResult"
 
 export const mapResult = <E1, D1, E2, D2>(
   result: Result<E1, D1>,
   transformFailure: (error: E1) => E2,
   transformSuccess: (data: D1) => D2,
-): Result<E2, D2> =>
-  result.isSuccess
-    ? { isSuccess: true, data: transformSuccess(result.data) }
-    : { isSuccess: false, error: transformFailure(result.error) }
+): Result<E2, D2> => {
+  const { createSuccess, createFailure } = createResult<E2, D2>()
+  return result.isSuccess
+    ? createSuccess(transformSuccess(result.data))
+    : createFailure(transformFailure(result.error))
+}
